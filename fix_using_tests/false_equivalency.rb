544698b07ea4115496877f8061d490309314fb03
Keith Williams <thestreetkw@hotmail.com>
2020-10-10 06:32:56 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,9 +13,9 @@ end
 
 def selection(num)
   if num = 1
-   "YUM YUM MUNCH MUNCH MUNCH"
+   print "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-   "HAM HAM HAM IN MY TUMMY"
+   print "HAM HAM HAM IN MY TUMMY"
   end
 end
 
