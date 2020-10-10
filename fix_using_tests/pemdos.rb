417c1a10162f9690bbd5b60a4d84c5f17c347711
Keith Williams <thestreetkw@hotmail.com>
2020-10-10 06:26:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,9 @@
 # don't forget to add: require 'pry'
 require 'pry'
 
-def snake_it_up(string, n=10)
+def snake_it_up(string)
   if string[0] == "s"
-    n * "s" + string
+    "s" * 10 + string
     binding.pry
   else
     string
